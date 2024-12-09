11bfc7e689bb3d64831b8f42ca535765315c59c4
Laker43311 <Laker43311@yandex.ru>
2024-12-09 19:53:12 +0500

    add interface & change methods names & fix role on front

---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
@@ -0,0 +1,16 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import ru.kata.spring.boot_security.demo.models.User;
+
+import java.util.List;
+
+public interface UserService {
+    public List<User> getAllUsers();
+    public User findByUsername(String username);
+    public User getUserById(long id);
+    public User findByEmail(String email);
+    public void updateUser(long id, User updatedUser);
+    public void deleteUser(Long id);
+    public void addUser(User user);
+
+}
